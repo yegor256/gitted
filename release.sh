e6ee0995df50b0a657a7b92f13e9ad1afa458ead
Yegor Bugayenko <yegor256@gmail.com>
2025-06-01 10:30:11 +0300

    #30: Refactor release script for improved readability.

--- release.sh
+++ release.sh
@@ -25,6 +25,7 @@ for f in "${versioned[@]}"; do
     sed -i "s/0\.0\.0/${tag}/g" "${f}"
     git add "${f}"
 done
+git commit --no-verify -m "version set to ${tag}"
 
 while IFS= read -r f; do
     n=$(basename "${f}")
@@ -32,7 +33,7 @@ while IFS= read -r f; do
     printf '%s=%s(cat << EOT\n%s\nEOT\n)\n\n%s' "help_${n}" "\$" "$(cat "${f}")" "$(cat "${base}/sub-scripts/intro.sh")" > "${base}/sub-scripts/intro.sh"
 done < <(find help -type f -name '*.txt')
 git add "${base}/sub-scripts/intro.sh"
-git commit -m "version set to ${tag}"
+git commit --no-verify -m "help messages moved into the intro.sh"
 
 pip install --progress-bar=off uv
 GITTED_BATCH=true GITTED_TESTING=true make -e
@@ -42,6 +43,7 @@ while IFS= read -r f; do
     while IFS= read -r line; do
         if [[ "${line}" =~ all\.sh ]]; then
             while IFS= read -r sub; do
+                echo "# Imported from $(basename "${sub}") when releasing:"
                 cat "${sub}"
             done < <(find "${base}/sub-scripts/" -type f -name '*.sh' -not -name 'all.sh')
         else
@@ -53,4 +55,9 @@ while IFS= read -r f; do
 done < <(find "${base}/scripts" -type f)
 
 uv --color=never build --no-build-logs
-uv --color=never run python -m twine upload dist/* -u __token__ -p "${token}"
+if [ -n "${token}" ]; then
+    uv --color=never run python -m twine upload dist/* -u __token__ -p "${token}"
+else
+    echo "The token is not provided, can't release"
+    exit 1
+fi
