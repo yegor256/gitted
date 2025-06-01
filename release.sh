3999671ab65cc7613b848b3bf83915bf46081f4c
Yegor Bugayenko <yegor256@gmail.com>
2025-06-01 10:24:51 +0300

    #30: Set version to specified tag and update file paths in release script.

--- release.sh
+++ release.sh
@@ -9,27 +9,33 @@ set -e -o pipefail
 tag=$1
 token=$2
 
+base=$(dirname "$0")
+
 if ! [[ "${tag}" =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
     echo 'The tag must be in SemVer'
     exit 1
 fi
 
-sed -i "s/0\.0\.0/${tag}/g" pyproject.toml
-git add pyproject.toml
-sed -i "s/0\.0\.0/${tag}/g" src/gitted/__init__.py
-git add src/gitted/__init__.py
-sed -i "s/0\.0\.0/${tag}/g" sub-scripts/intro.sh
-git add sub-scripts/intro.sh
-git commit -m "version set to ${tag}"
-
-pip install --progress-bar=off uv
-GITTED_BATCH=true GITTED_TESTING=true make -e
+versioned=(
+    "${base}/pyproject.toml"
+    "${base}/src/gitted/__init__.py"
+    "${base}/sub-scripts/intro.sh"
+)
+for f in "${versioned[@]}"; do
+    sed -i "s/0\.0\.0/${tag}/g" "${f}"
+    git add "${f}"
+done
 
 while IFS= read -r f; do
     n=$(basename "${f}")
     n=${n%.*}
-    printf '%s=%s(cat << EOT\n%s\nEOT\n)\n\n%s' "help_${n}" "\$" "$(cat "${f}")" "$(cat sub-scripts/intro.sh)" > sub-scripts/intro.sh
+    printf '%s=%s(cat << EOT\n%s\nEOT\n)\n\n%s' "help_${n}" "\$" "$(cat "${f}")" "$(cat "${base}/sub-scripts/intro.sh")" > "${base}/sub-scripts/intro.sh"
 done < <(find help -type f -name '*.txt')
+git add "${base}/sub-scripts/intro.sh"
+git commit -m "version set to ${tag}"
+
+pip install --progress-bar=off uv
+GITTED_BATCH=true GITTED_TESTING=true make -e
 
 while IFS= read -r f; do
     temp=$(mktemp)
@@ -37,14 +43,14 @@ while IFS= read -r f; do
         if [[ "${line}" =~ all\.sh ]]; then
             while IFS= read -r sub; do
                 cat "${sub}"
-            done < <(find "$(dirname "$0")" -type f -name '*.sh' -not -name "all.sh")
+            done < <(find "${base}/sub-scripts/" -type f -name '*.sh' -not -name 'all.sh')
         else
             echo "${line}"
         fi
     done < "${f}" > "${temp}"
     mv "${temp}" "${f}"
     chmod a+x "${f}"
-done < <(find scripts -type f)
+done < <(find "${base}/scripts" -type f)
 
 uv --color=never build --no-build-logs
 uv --color=never run python -m twine upload dist/* -u __token__ -p "${token}"
