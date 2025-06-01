773d15981d305bcf18ffcd0f1bb98b9466edba6a
Rultor.com <gpg@rultor.com>
2025-06-01 07:51:26 +0000

    version set to 0.0.14

--- sub-scripts/intro.sh
+++ sub-scripts/intro.sh
@@ -17,7 +17,7 @@ if [ "$1" = '--help' ]; then
 fi
 
 if [ -z "${GITTED_INTRODUCED}" ]; then
-    printf "\e[1mGitted\e[0m 0.0.0 (https://github.com/yegor256/gitted)\n"
+    printf "\e[1mGitted\e[0m 0.0.14 (https://github.com/yegor256/gitted)\n"
     GITTED_INTRODUCED=true
     export GITTED_INTRODUCED
 fi
