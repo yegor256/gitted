012d1e0ae072075cbaa26d962629adc77734ae13
Rultor.com <gpg@rultor.com>
2025-06-18 13:40:09 +0000

    help messages moved into the intro.sh

--- sub-scripts/intro.sh
+++ sub-scripts/intro.sh
@@ -1,3 +1,37 @@
+help_pull=$(cat << EOT
+Usage: pull
+
+We pull from the upstream branch and then also from the upstream repository.
+EOT
+)
+
+help_push=$(cat << EOT
+Usage: push [<message> | <branch>]
+
+You either provide a commit message or a name of the branch you push to. If the
+argument is a single integer, we treat it as a branch name. Everything else
+is treated as a plain message.
+EOT
+)
+
+help_branch=$(cat << EOT
+Usage: branch <name>
+
+You either provide a "message" or a name of the branch you push to. If the
+argument is a single integer, we treat it as a branch name. Everything else
+is treated as a plain message.
+EOT
+)
+
+help_commit=$(cat << EOT
+Usage: commit [<message> | <branch>]
+
+You either provide a commit message or a name of the branch you push to. If the
+argument is a single integer, we treat it as a branch name. Everything else
+is treated as a plain message.
+EOT
+)
+
 #!/bin/bash
 # SPDX-FileCopyrightText: Copyright (c) 2025 Yegor Bugayenko
 # SPDX-License-Identifier: MIT
