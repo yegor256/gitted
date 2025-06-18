e388d5c4c31f913543dfb24e3fd3c3b19974a50e
Andrey Chernykh <andreychernykh04@gmail.com>
2025-06-18 13:49:44 +0300

    #39: update shellcheck directive

--- sub-scripts/all.sh
+++ sub-scripts/all.sh
@@ -6,9 +6,9 @@ set -e -o pipefail
 
 scripts="$(dirname "$0")/../sub-scripts"
 
-# shellcheck disable=SC1090
+# shellcheck disable=SC1091
 . "${scripts}/intro.sh"
-# shellcheck disable=SC1090
+# shellcheck disable=SC1091
 . "${scripts}/commons.sh"
-# shellcheck disable=SC1090
+# shellcheck disable=SC1091
 . "${scripts}/sanity.sh"
