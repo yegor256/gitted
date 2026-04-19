90c1731cd4a460ed85ad18227ede3d229179f4a8
Yegor Bugayenko <yegor256@gmail.com>
2026-04-19 14:35:00 +0300

    #60: Update submodules initialization and submodule ignoring.

---
+++ tests.sh/pulls-when-submodule-has-local-changes.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+# SPDX-FileCopyrightText: Copyright (c) 2025 Yegor Bugayenko
+# SPDX-License-Identifier: MIT
+
+set -ex -o pipefail
+
+tmp=$(pwd)
+base=$(realpath "$(dirname "$0")/..")
+
+rm -rf sub
+git init sub --initial-branch=master
+cd sub || exit 1
+git config user.email "sub@zerocracy.com"
+git config user.name "Sub Author"
+echo 'original' > pom.xml
+git add pom.xml
+git commit --no-verify -am "submodule initial"
+cd .. || exit 1
+
+rm -rf origin
+git init --bare origin --initial-branch=master
+cd origin || exit 1
+git config uploadpack.allowFilter true
+cd .. || exit 1
+
+rm -rf foo
+git clone "file://${tmp}/origin" foo
+cd foo || exit 1
+git config user.email "foo@zerocracy.com"
+git config user.name "Foo Author"
+touch root.txt
+git add root.txt
+git commit --no-verify -am "foo initial"
+git -c protocol.file.allow=always submodule add "file://${tmp}/sub" sub
+git commit --no-verify -am "added submodule"
+git push origin master
+cd .. || exit 1
+
+cd sub || exit 1
+echo 'upstream update' > pom.xml
+git commit --no-verify -am "upstream change"
+cd .. || exit 1
+
+cd foo || exit 1
+echo 'LOCAL ACCIDENTAL CHANGE' > sub/pom.xml
+
+env "GITTED_TESTING=true" "GIT_ALLOW_PROTOCOL=file" \
+    "${base}/scripts/pull" 2>&1 | tee "${tmp}/log.txt"
+
+grep 'upstream update' sub/pom.xml
+! grep -q 'LOCAL ACCIDENTAL CHANGE' sub/pom.xml
